8166770a9580694134bad085311a15b300bc240d
Jan Pražák <janp@horasion.eu>
2022-09-07 22:42:21 +0200

    Create array_struct-demo.c

---
+++ pointers/array_struct-demo.c
@@ -0,0 +1,50 @@
+#include <stdio.h>	/* printf */
+#include <stdlib.h> /* malloc */
+#include <math.h>
+
+#include "array_struct.h"
+
+int main()
+{
+	IntArray arr;
+	FloatArray squareRoots;
+	unsigned int i;
+
+	arr.size = 6;
+	arr.data = malloc(arr.size * sizeof *arr.data);
+
+	squareRoots.size = 10;
+	squareRoots.data = malloc(squareRoots.size * sizeof *squareRoots.data);
+
+	for (i = 0; i < arr.size; i++)
+	{
+		arr.data[i] = i * 10;
+	}
+
+	for (i = 0; i < arr.size; i++)
+	{
+		printf("%d ", arr.data[i]);
+		/* prints:  0 10 20 30 40 50 */
+	}
+
+	printf("\n");
+
+	for (i = 0; i < squareRoots.size; i++)
+	{
+		squareRoots.data[i] = sqrt(i);
+	}
+
+	for (i = 0; i < squareRoots.size; i++)
+	{
+		printf("%f ", squareRoots.data[i]);
+		/* prints:
+				0.000000 1.000000 1.414214 1.732051 2.000000 2.236068 2.449490 2.645751 2.828427 3.000000 */
+	}
+
+	printf("\n");
+
+	free(arr.data);
+	free(squareRoots.data);
+
+	return 0;
+}
